9a83f14f42399c8a7dcd6d5bf769f107acf23abf
rajaisha <156165194+rajaisha@users.noreply.github.com>
2024-01-10 11:51:48 +0530

    Add files via upload

---
+++ client1.py
@@ -0,0 +1,15 @@
+from socket import
+client=connect()
+client.connect(("192.168.206.109",9000))
+print(client.recv(1024).decode())
+client.send(bytes("WELCOME IN MY WORLD","UFT-8"))
+While True:
+    sm=(client.recv(1024)).decode()
+    print("server:",sm)
+    cm=input("client:")
+    client.send(bytes(cm,"uft-8"))
+    if sm=="bye" or "BYE":
+        print("
+        break
+    else:
+        continue
